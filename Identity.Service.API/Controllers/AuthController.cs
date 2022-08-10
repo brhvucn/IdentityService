def126f69b3af486d58db9844eb2801d4d516e15
Brian Hvarregaard <brian@median.dk>
2022-08-10 12:41:01 +0200

    First commit, setup

---
+++ Identity.Service.API/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Service.API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
